feaa51f3c84d94032d58ba072c78a2198b075a67
Milan Ma <milanmatejicdev@gmail.com>
2021-03-14 20:14:58 +0100

    created html structure, styled all and declared all variables in js file

---
+++ app.js
@@ -0,0 +1,8 @@
+const btn1 = document.querySelector(".btn-1");
+const btn2 = document.querySelector(".btn-2");
+const btn3 = document.querySelector(".btn-3");
+const timer = document.querySelector(".timer");
+
+btn1.addEventListener("click", function () {
+  //console.log("Button is clicked");
+});
